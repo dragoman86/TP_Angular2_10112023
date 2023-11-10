6e8850c5308e0bfc5394b33c5d377e2dcd660408
dragoman86 <walid.benna@gmail.com>
2023-11-10 10:24:32 +0100

    third commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Personne } from './classes/personne';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'tpangular_2';
+  personne: Personne;
+  notes: number[] = [10, 15, 8, 12, 20, 17, 14, 9, 13, 16];
+  personnes: Array<Personne> = [
+    new Personne(100, 'tounsi', 'Mohamed'),
+    new Personne(101, 'tounsi2', 'Mohamed2'),
+    new Personne(102, 'tounsi3', 'Mohamed3'),
+    new Personne(103, 'tounsi4', 'Mohamed4')
+  ];
+
+  constructor() {
+    // Créer une instance de la classe Personne
+    this.personne = new Personne(1, 'Benna', 'Walid');
+  }
 }
+
---
+++ src/app/classes/personne.ts
@@ -0,0 +1,11 @@
+export class Personne {
+    num: number;
+    nom: string;
+    prenom: string;
+  
+    constructor(num: number, nom: string, prenom: string) {
+      this.num = num;
+      this.nom = nom;
+      this.prenom = prenom;
+    }
+  }
